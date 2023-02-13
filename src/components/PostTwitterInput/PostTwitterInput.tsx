d2cca1ef50503c8d0da8c49d9f8b439bab79f045
Ryan Hinchliffe <49229804+ryboh1@users.noreply.github.com>
2023-02-13 11:50:53 +0000

    created the post to twitter component with an input that will be able to post comments to twitter

--- src/components/PostTwitterInput/PostTwitterInput.tsx
+++ src/components/PostTwitterInput/PostTwitterInput.tsx
@@ -20,7 +20,6 @@ const PostTwitterInput: FC<IPostTwitterInput> = () => {
                         </InputAdornment>
                     ),
                 }}
-                type="search"
                 variant="standard"
             />
         </div>
